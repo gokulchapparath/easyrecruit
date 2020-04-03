f28020e4532104aaa8620c1c61891402c8aa0f2f
gokulchapparath <gokulchapparath@gmail.com>
2020-04-03 22:05:33 +0530

    minor changes

--- easy-recruit/src/app/services/cv-analysis.service.ts
+++ easy-recruit/src/app/services/cv-analysis.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Idata } from '../cvanalysis';
 
@@ -8,10 +8,25 @@ import { Idata } from '../cvanalysis';
   providedIn: 'root'
 })
 export class CvAnalysisService {
-  private _url:string="/assets/data/cvanaylsis.json";
+  private _url: string = "/assets/data/cvanaylsis.json";
+  // public httpOptions = {
+  //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  // };
   constructor(private http: HttpClient) { }
   // readonly url="./modules/cv-analysis/tabledata.json";
-  getData():Observable<Idata[]>{
+  getData(): Observable<Idata[]> 
+  {
     return this.http.get<Idata[]>(this._url);
   }
+
+  // getId(id: number): Observable<Idata> {
+  //   const url = `${this._url}/${id}`;
+  //   return this.http.get<Idata>(url);
+  // }
+
+  // updateStatus (hero: Idata): Observable<any> {
+    
+  //   return this.http.put(this._url, hero, this.httpOptions);
+  // }
+  
 }
