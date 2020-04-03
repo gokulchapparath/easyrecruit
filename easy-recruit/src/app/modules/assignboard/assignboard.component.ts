02d15ed46e3de7251f9c492726d2cd05ec667994
gokulchapparath <gokulchapparath@gmail.com>
2020-04-03 22:02:52 +0530

    Assign board table Added

--- easy-recruit/src/app/modules/assignboard/assignboard.component.ts
+++ easy-recruit/src/app/modules/assignboard/assignboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AssignboardService} from 'src/app/services/assignboard.service';
 @Component({
   selector: 'app-assignboard',
   templateUrl: './assignboard.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignboardComponent implements OnInit {
 
-  constructor() { }
-  mydashboardstyle="border-left: 5px solid rgb(245,40,81,100%);"
+  constructor(private _AssignboardService : AssignboardService) { }
+  public list=[];
   ngOnInit(): void {
+    this._AssignboardService.getData()
+    .subscribe(data =>this.list=data);
   }
-
+  mydashboardstyle="border-left: 5px solid rgb(245,40,81,100%);"
 }
